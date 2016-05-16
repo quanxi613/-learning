16872d055143eb14fe09c7869149119f28c8ecde
quanxi613 <quanxi613@126.com>
2016-05-16 16:49:36 +0800

    first

---
+++ rem-cal.js
@@ -0,0 +1,2 @@
+// 无线端计算rem
+!function(e){function t(){var t=i.clientWidth;e.rem=t/10,/ZTE U930_TD/.test(navigator.userAgent)&&(e.rem=1.13*e.rem),r.innerHTML="html{font-size:"+e.rem+"px!important;}"}var n,i=document.documentElement,r=document.createElement("style"),n=e.devicePixelRatio||1;i.firstElementChild.appendChild(r),i.setAttribute("data-dpr",n),e.dpr=n,e.addEventListener("resize",function(){t()},!1),e.addEventListener("pageshow",function(e){e.persisted&&t()},!1),t()}(window);
